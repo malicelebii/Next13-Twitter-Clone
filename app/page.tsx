6e2b47447b8af6d67345b1651756c7cfd805500d
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-09-12 09:45:16 +0300

    Fetch all posts on client and show them in timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import { SessionProvider } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import Post from "../components/post";
 import Timeline from "@/components/timeline";
 import LeftSidebar from "@/components/left-sidebar";
 
-export default async function Home() {
+export default async function App({}) {
   return (
     <div className="flex justify-between m-5 mx-auto bg-black w-2/3">
       <div className="">
         <LeftSidebar />
       </div>
       <div>
-        <Timeline userId={"64f83ca89e819bef241e496d"}/>
+        <Timeline />
       </div>
       <div>
       <LeftSidebar />
--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,14 +1,23 @@
-import Tweets, { TweetType } from "@/components/tweets";
+import { cookies } from "next/dist/client/components/headers";
+import Tweets from "./tweets";
 
-export default async function Timeline({ userId }: { userId: string }) {
-  const list = await fetch(
-    `http://localhost:3000/api/posts/followings/${userId}`
-  );
-  const newList = await list.json();
+export default async function Timeline() {
+  // const session = await getServerSession(authOptions);
+  const cooks =cookies();
+  const token =cooks.get("next-auth.session-token")?.value;
+
+  const data = await fetch(`http://localhost:3000/api/posts/followings`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  const list = await data.json()
+
+
 
   return (
     <div className="bg-blue-300 p-5 flex justify-center mx-auto">
-      <div className=""><Tweets tweets={newList.posts} /></div>
+      <div className=""><Tweets tweets={list.posts} /></div>
     </div>
   );
 }
