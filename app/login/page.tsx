df7991d8d482e7b0cce2cb706c4d8512e07b99dd
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-09-13 08:51:47 +0300

    Change logo on pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export default function Login() {
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           <Link href="/">
             <Image
-              src="/logo.png"
+              src="/x.png"
               priority
               alt="Logo"
               className="h-10 w-10 rounded-full"
