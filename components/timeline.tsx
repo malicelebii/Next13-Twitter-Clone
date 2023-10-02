63a857568aaaa4b2ac62ad4a0e6d42d17dbda18b
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-02 08:46:58 +0300

    Edit timeline to get tweets by session based logic

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,23 +1,41 @@
 import { cookies } from "next/dist/client/components/headers";
 import Tweets from "./tweets";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Image from "next/image";
+import Link from "next/link";
+import SignOut from "./sign-out";
+import { redirect } from 'next/navigation'
 
 export default async function Timeline() {
-  // const session = await getServerSession(authOptions);
-  const cooks =cookies();
-  const token =cooks.get("next-auth.session-token")?.value;
+  const session = await getServerSession(authOptions);
+  let data;
+  if (session) {
+    const cooks = cookies();
+    const token = cooks.get("next-auth.session-token")?.value;
 
-  const data = await fetch(`http://localhost:3000/api/posts/followings`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-  const list = await data.json()
-
-
+    data = await fetch(`http://localhost:3000/api/posts/followings`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  const list = await data?.json();
 
   return (
-    <div className="bg-blue-300 p-5 flex justify-center mx-auto">
-      <div className=""><Tweets tweets={list.posts} /></div>
+    <div className=" p-5 flex justify-center mx-auto bg-black">
+      {session ? (
+        <div>
+          <div className="">
+            <Tweets tweets={list.posts} />
+          </div>
+          <div className=" flex flex-col space-y-5 justify-center items-center">
+            <SignOut />
+          </div>
+        </div>
+      ) : (
+        redirect("/login")
+      )}
     </div>
   );
 }
