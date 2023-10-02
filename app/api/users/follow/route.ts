33ca6c7b836025dabdbedfcf44476556293989f5
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-02 08:43:56 +0300

    Edit follow route to get user from next-auth

--- app/api/users/follow/route.ts
+++ app/api/users/follow/route.ts
@@ -1,24 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { getUserId } from "helpers/user";
 
 export const POST = async (req: NextRequest) => {
-    
   const res = await req.json();
 
-  const { userId, followerId } = res;
+  const userId = await getUserId(req);
+
+  const { followId } = res;
 
   const user = await prisma?.user.update({
     where: {
       id: userId,
     },
     data: {
-      followedBy: {
-        connect: { id: followerId },
+      following: {
+        connect: { id: followId },
       },
     },
   });
 
-
-
-  return NextResponse.json(user)
+  return NextResponse.json(user);
 };
