930ab224f1a90d038238d540d8db8a9a85cf1571
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-02 08:38:24 +0300

    Add write-post component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Timeline from "@/components/timeline";
 import LeftSidebar from "@/components/left-sidebar";
+import WritePost from "@/components/write-post";
 
 export default async function App({}) {
   return (
-    <div className="flex justify-between m-5 mx-auto bg-black w-2/3">
-      <div className="">
-        <LeftSidebar />
-      </div>
+    <div className="flex justify-center m-5 mx-auto">
+      {/* <div></div> */}
       <div>
+        <LeftSidebar />
+        </div>
+      <div className="flex flex-col justify-center items-center">
+        <WritePost />
         <Timeline />
       </div>
       <div>
