aafbc837006dedfbf0067d19f5905d133c64c2a0
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-05 09:44:24 +0300

    Add a function which checks the tweet is liked by signing user +Add route to get user's likes

--- app/api/users/like/route.ts
+++ app/api/users/like/route.ts
@@ -2,6 +2,21 @@ import prisma from "@/lib/prisma";
 import { getUserId } from "helpers/user";
 import { NextRequest, NextResponse } from "next/server";
 
+export const GET =async (req: NextRequest) => {
+    const userId = await getUserId(req);
+
+    const user = await prisma.user.findUnique({
+      where:{
+        id:userId
+      },
+      include:{
+        likedPosts:true
+      }
+    })
+    return NextResponse.json(user);
+}
+
+
 export const POST = async (req: NextRequest) => {
   const userId = await getUserId(req);
   const res = await req.json();
@@ -20,3 +35,4 @@ export const POST = async (req: NextRequest) => {
 
   return NextResponse.json(user);
 };
+
--- helpers/tweetInteractions.ts
+++ helpers/tweetInteractions.ts
@@ -1,9 +1,26 @@
+import { cookies } from "next/dist/client/components/headers";
+
 export const likeTweet = async (postId) => {
   const post = await fetch("/api/users/like", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ postId}),
+    body: JSON.stringify({ postId }),
   }).then((r) => r.json());
+};
+
+export const IsTweetLiked = async (postId) => {
+  const cooks = cookies();
+  const token = cooks.get("next-auth.session-token")?.value;
+
+  let data = await fetch(`http://localhost:3000/api/users/like`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((r) => r.json());
+
+  const filteredForIds = data.likedPosts.map((post) => post.id);
+
+  return filteredForIds.includes(postId);
 };
