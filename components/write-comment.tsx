1909dd2ca91cb4dfb013c9ecc992f99917a5c1a5
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-10 11:32:55 +0300

    Add write comment component

---
+++ components/write-comment.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Image from "next/image";
+import {  useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function WriteComment({id}) {
+  const [content, setContent] = useState("second");
+  const router = useRouter();
+
+  const onContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    // e.preventDefault();
+    try {
+      const response = await fetch(`/api/posts/${id}/comments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content }),
+      });
+  
+      if (response.ok) {
+        router.refresh();
+      }  
+    } catch (error) {
+      
+    }
+    
+  };
+
+  return (
+    <div className="flex justify-center p-10 w-full">
+      <div className="rounded-full border p-3 w-16 h-16">
+        <Image
+          src="/x.png" // Profil fotoğrafı URL'si
+          alt="Profil Fotoğrafı"
+          className="object-cover w-full h-full "
+          width={15}
+          height={15}
+        />
+      </div>
+      <div className=" w-full">
+        <form className="px-2" onSubmit={onSubmit}>
+          <textarea
+            className="placeholder:italic place-items-center placeholder:text-slate-400 block bg-white h-28 w-full  p-2 shadow-sm border rounded-lg sm:text-md resize-none"
+            placeholder="Ne düşünüyorsun"
+            onChange={onContentChange}
+          />
+          <button
+            type="submit"
+            className="float-right rounded-full mt-2 bg-blue-300 p-2"
+          >
+            Gönder
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
