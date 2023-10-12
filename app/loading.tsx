56870274e95284e02d818cbc9e17ef10e7b24493
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-12 16:47:28 +0300

    Create loading component as a root component

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="border-t-4 border-b-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loading;
