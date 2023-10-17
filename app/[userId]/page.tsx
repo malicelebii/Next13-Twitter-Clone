3639fd29dcd3df93441b50d901cd7471bc04e304
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-17 14:46:45 +0300

    Edit user profile page

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,10 +1,10 @@
 import FollowButton from "@/components/follow-button";
-import LeftSidebar from "@/components/left-sidebar";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 import UploadImage from "@/components/upload-image";
 import Link from "next/link";
+import ProfileItems from "@/components/profile-items";
+import UserTweets from "@/components/user-tweets";
 export default async function UserProfile({
   params,
 }: {
@@ -12,52 +12,74 @@ export default async function UserProfile({
 }) {
   const data = await fetch(
     `http://localhost:3000/api/users/profile/${params.userId}`,
-    { method: "GET" ,cache:"no-store"}
+    { method: "GET", cache: "no-store" }
   );
   const user = await data.json();
-const session =await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
   let userId;
   if (session) {
-    userId=session.token.sub;
+    userId = session.token.sub;
   }
 
- 
-
   return (
-    <div className="flex justify-between m-5 mx-auto gap-10 max-w-5xl  bg-slate-400">
-      <div className="bg-gray-100  flex items-center justify-center w-full">
-        <div className="bg-white p-6 rounded-lg shadow-md w-full">
-          <div className="flex items-center space-x-4">
-            <UploadImage imgSrc={user.profileImgUrl}/>
-           
-            <div>
-              <h1 className="text-2xl font-semibold">{user.name}</h1>
-              <p className="text-gray-600">@johndoe</p>
-            </div>
-          </div>
-          <p className="mt-4 text-gray-800">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-            odio.
-          </p>
-          <div className="mt-4">
-            <div className="flex">
-              <div className="mr-2">
-              <Link href={`/${params.userId}/followers`} className="hover:underline">  <strong>{user.followedByIDs?.length}</strong> Takipçi</Link>
-              </div>
+    <>
+      <div className="flex justify-between m-5 mx-auto gap-10 max-w-5xl  bg-slate-400">
+        <div className="bg-gray-100  flex items-center justify-center w-full">
+          <div className="bg-white p-6 rounded-lg shadow-md w-full">
+            <div className="flex items-center space-x-4">
+              <UploadImage imgSrc={user.profileImgUrl} />
+
               <div>
-              <Link href={`/${params.userId}/following`} className="hover:underline"> <strong>{user.followingIDs?.length}</strong> Takip Edilen </Link>
+                <h1 className="text-2xl font-semibold">{user.name}</h1>
+                <p className="text-gray-600">@johndoe</p>
               </div>
             </div>
+            <p className="mt-4 text-gray-800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio.
+            </p>
+            <div className="mt-4">
+              <div className="flex items-center">
+                <div className="mr-2">
+                  <Link
+                    href={`/${params.userId}/followers`}
+                    className="hover:underline"
+                  >
+                    {" "}
+                    <strong>{user.followedByIDs?.length}</strong> Takipçi
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    href={`/${params.userId}/following`}
+                    className="hover:underline"
+                  >
+                    {" "}
+                    <strong>{user.followingIDs?.length}</strong> Takip Edilen{" "}
+                  </Link>
+                </div>
+                <div>
+                  {userId !== user.id &&
+                    !user.followedByIDs.includes(userId) && (
+                      <FollowButton
+                        followId={params.userId}
+                        buttonType={"follow"}
+                      />
+                    )}
+                  {user.followedByIDs.includes(userId) && (
+                    <FollowButton
+                      followId={params.userId}
+                      buttonType={"unfollow"}
+                    />
+                  )}
+                </div>
+              </div>
+              <ProfileItems userId={params.userId}/>
+  
+            </div>
           </div>
-          { (userId !== user.id && !user.followedByIDs.includes(userId))&&
-        <FollowButton followId={params.userId} buttonType={"follow"}/>         
-      }
-      {
-        user.followedByIDs.includes(userId) && 
-        <FollowButton followId={params.userId} buttonType={"unfollow"} />        
-      }
         </div>
       </div>
-    </div>
+    </>
   );
 }
