f4a9255e86ab029804fa036c8ec249aa4cda33be
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-10-17 14:47:40 +0300

    Add posts field to get user route

--- app/api/users/profile/[userId]/route.ts
+++ app/api/users/profile/[userId]/route.ts
@@ -1,30 +1,32 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest,{params}:{params:{userId:string}}) => {
-  let user ;
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { userId: string } }
+) => {
+  let user;
   try {
-     user = await prisma.user.findUnique({
+    user = await prisma.user.findUnique({
       where: {
         id: params.userId,
       },
-      select:{
-        id:true,
-        email:true,
-        name:true,
-        followedByIDs:true,
-        followingIDs:true,
-        profileImgUrl:true,
-        commments:true,
-        followedBy:true,
-        following:true
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        posts: { orderBy: { createdAt: "desc" } },
+        followedByIDs: true,
+        followingIDs: true,
+        profileImgUrl: true,
+        commments: true,
+        followedBy: true,
+        following: true,
       },
-    });  
+    });
   } catch (error) {
     console.log(error);
-    
   }
-  
 
   return NextResponse.json(user);
 };
